0e8c84d8d92a98654381a13b3ee4c2539f161c98
AdityaA2010 <118632582+AdityaA2010@users.noreply.github.com>
2023-05-14 09:49:10 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,65 +1,128 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+const Constraint = Matter.Constraint;
 
-var ball, groundObj, leftSide, rightSide
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var playerArrows = [];
+var board1, board2;
+var numberOfArrows = 10;
 
-function preload()
-{
-	
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
 }
 
 function setup() {
-	createCanvas(1600, 700);
+  canvas = createCanvas(windowWidth, windowHeight);
 
+  engine = Engine.create();
+  world = engine.world;
 
-	engine = Engine.create();
-	world = engine.world;
+  playerBase = new PlayerBase(300, 500, 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
-	//Create the Bodies Here.
-
-	var ball_options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0,
-		density:1.2 }
-
-	ball = Bodies.circle(200,100,20,ball_options);
-    World.add(world,ball);
-
-	groundObj = new ground(width/2,670,width,20);
-	leftSide = new ground(1100,600,20,120);
-	rightSide = new ground(1300,600,20,120);
-
-	Engine.run(engine);
-	}
-	
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  groundObj.display();
-  leftSide.display();
-  rightSide.display();
-
-  ellipse(ball.position.x,ball.position.y,20,20);
+  board1 = new Board(width - 300, 330, 50, 200);
+  board2 = new Board(width - 550, height - 300, 50, 200);
 }
 
+function draw() {
+  background(backgroundImg);
 
+  Engine.update(engine);
 
-  function keyPressed(){
-	if(keyCode === UP_ARROW) {
-	 Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
-	}
- 
- }
- 
+  playerBase.display();
+  player.display();
+  playerArcher.display();
 
+  board1.display();
+  board2.display();
 
+  for (var i = 0; i < playerArrows.length; i++) {
+    if (playerArrows[i] !== undefined) {
+      playerArrows[i].display();
 
+       var board1Collision = Matter.SAT.collides(
+        board1.body,
+        playerArrows[i].body
+      ); 
 
+      /* var board1Collision = collides(
+        board1.body,
+        playerArrows[i].body
+      ); */
 
+      /* var board1Collision = SAT.collides(
+        board1.body,
+        playerArrows[i].body
+      ); */
+
+      /* var board1Collision = Matter(
+        board1.body,
+        playerArrows[i].body
+      ); */
+      
+      var board2Collision = Matter.SAT.collides(
+        board2.body,
+        playerArrows[i].body
+      );
+
+      if (board1Collision.collided || board2Collision.collided) {
+        console.log("Collided");
+      }
+
+      var posX = playerArrows[i].body.position.x;
+      var posY = playerArrows[i].body.position.y;
+
+      if (posX > width || posY > height) {
+        if (!playerArrows[i].isRemoved) {
+          playerArrows[i].remove(i);
+        }
+      }
+    }
+  }
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+  // Arrow Count
+  fill("#FFFF");
+  textAlign("center");
+  textSize(30);
+  text("Remaining Arrows : " + numberOfArrows, 200, 100);
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    if (numberOfArrows > 0) {
+      var posX = playerArcher.body.position.x;
+      var posY = playerArcher.body.position.y;
+      var angle = playerArcher.body.angle;
+
+      var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+      Matter.Body.setAngle(arrow.body, angle);
+      playerArrows.push(arrow);
+      numberOfArrows -= 1;
+    }
+  }
+}
+
+function keyReleased() {
+  if (keyCode === 32) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+}
